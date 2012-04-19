82532129a7a621ca80a185bae512fa46b86687d9
igor.zc@gmail.com <igor.zc@gmail.com@9caf99f7-9d2f-0410-974c-5db699c9257e>
2012-04-19 21:07:00 +0000

    added folder /tools

---
+++ tools/createInstallationXpiFile.cmd
@@ -0,0 +1,28 @@
+
+rem
+rem prepare SetupFiles folder
+rem
+rmdir /S /Q SetupFiles
+mkdir SetupFiles
+xcopy /E /y ..\sources\*.* SetupFiles\*.*
+
+copy zip.exe SetupFiles\chrome\
+copy create_hcalendar.jar.cmd SetupFiles\chrome\
+chdir SetupFiles\chrome\
+call create_hcalendar.jar.cmd
+chdir ..\..\
+
+rmdir /S /Q SetupFiles\chrome\content
+rmdir /S /Q SetupFiles\chrome\locale
+del /Q SetupFiles\chrome\
+zip.exe
+del /Q SetupFiles\chrome\create_hcalendar.jar.cmd
+
+copy zip.exe SetupFiles
+chdir SetupFiles
+zip.exe -r hcalendar1.0.5.15-fx+fl+mz+ns+tb.xpi *.* -x *.exe
+chdir ..
+
+dir SetupFiles\hcalendar1.0.5.15-fx+fl+mz+ns+tb.xpi
+xcopy /Y SetupFiles\hcalendar1.0.5.15-fx+fl+mz+ns+tb.xpi .\
+pause
